4666ed657f075c1df5ae437e2bea40612eb4cab0
Harshana N Weerasingha <73938301+Harshana369@users.noreply.github.com>
2024-03-22 22:26:23 +0530

    hjhjhjh

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -36,7 +36,7 @@ export const generateMetadata = async ({
       siteName: siteConfig.siteName,
       images: [
         {
-          url: "https://localhost:3000/opengraph-image.png",
+          url: "https://bhwaya.com/opengraph-image.png",
           width: 1200,
           height: 628,
         },
