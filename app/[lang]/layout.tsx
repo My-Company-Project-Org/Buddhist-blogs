c61ce54699b35c5bab8ab71b5005152811f5e579
Harshana N Weerasingha <73938301+Harshana369@users.noreply.github.com>
2024-03-28 21:48:37 +0530

    change scripte

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -65,12 +65,13 @@ export default function RootLayout({
       {/* Google Analytics Script */}
       <Script
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-11Y5YESB1F"
+        src="https://www.googletagmanager.com/gtag/js?id=G-Z4YJZ1BJ3V"
       ></Script>
-      <Script id="google-analytics">{`window.dataLayer = window.dataLayer || [];
+
+      <Script id="google-analytics">{`  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-11Y5YESB1F');`}</Script>
+  gtag('config', 'G-Z4YJZ1BJ3V');`}</Script>
       <body className={inter.className}>
         {/* @ts-expect-error Async Server Component */}
         <Navigation locale={lang} />
